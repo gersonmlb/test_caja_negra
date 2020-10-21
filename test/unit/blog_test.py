cc2e1a59fd2d9be2fdfab91f8d6443473cf7a0a7
gersonmlb <gerson_159@outlook.es>
2020-10-21 08:13:18 -0500

    Test Blog

---
+++ test/unit/blog_test.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+from blog import Blog
+
+
+class BlogTest(TestCase):
+    def test_create_blog(self):
+        b = Blog('Nico Content', 'Nico')
+        
+        self.assertEqual('Nico Content',b.title)
+        self.assertEqual('Nico', b.author)
+        self.assertEqual([], b.posts)
+    
+    def test_repr(self):
+        b = Blog('Test', 'Nico Author')
+        b1 = Blog('My Day', 'Nico')
+
+        self.assertEqual(b.__repr__(), 'Test By Nico Author (0 posts)')
+        self.assertEqual(b1.__repr__(), 'My Day By Nico (0 posts)')
+    
+    def test_repr_multiple_posts(self):
+        b = Blog('Test', 'Nico Author')
+        b.posts = ['test']
+        
+        b1 = Blog('My Day', 'Nico')
+        b1.posts = ['test', 'another']
+        
+        self.assertEqual(b.__repr__(), 'Test By Nico Author (1 post)')
+        self.assertEqual(b1.__repr__(), 'My Day By Nico (2 posts)')
+    
